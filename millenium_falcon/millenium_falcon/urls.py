a96bba5af9efa2c34dddaf9788f47f4b9540069e
Icaro Gavazza <icarodgl@gmail.com>
2017-05-10 17:37:11 -0300

    reacriado com o nome correto

--- millenium_falcon/millenium_falcon/urls.py
+++ millenium_falcon/millenium_falcon/urls.py
@@ -1,4 +1,4 @@
-"""millenium_facom URL Configuration
+"""millenium_falcon URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- millenium_falcon/millenium_falcon/wsgi.py
+++ millenium_falcon/millenium_falcon/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for millenium_facom project.
+WSGI config for millenium_falcon project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "millenium_facom.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "millenium_falcon.settings")
 
 application = get_wsgi_application()
