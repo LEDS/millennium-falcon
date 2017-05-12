71973bbf05355f98b7fc10db582e124f92de4d67
Icaro Gavazza <icarodgl@gmail.com>
2017-05-12 14:44:04 -0300

    classes criadas

--- millenium_falcon/gestao/admin.py
+++ millenium_falcon/gestao/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Pessoa, Professor, Servidor, Aluno, Representante_parceiro
+admin.site.register(Pessoa)
+admin.site.register(Professor)
+admin.site.register(Servidor)
+#admin.site.register(Alunos)
+admin.site.register(Representante_parceiro)
 
-# Register your models here.
+class AlunoAdmin(admin.ModelAdmin):
+  list_display = ['nome','papel','entrada_leds','periodo']
+  list_filter = ['papel']
+
+admin.site.register(Aluno,AlunoAdmin)
--- millenium_falcon/gestao/models.py
+++ millenium_falcon/gestao/models.py
@@ -1,5 +1,31 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=255, null=False)
+    papel = models.CharField(max_length=255, null=False)
+    entrada_leds = models.DateField()
+    saida_leds = models.DateField()
+
+class Professor(Pessoa):
+    email = models.CharField(max_length=255, null=False)
+
+
+class Servidor(Pessoa):
+    email = models.CharField(max_length=255, null=False)
+
+class Aluno(Pessoa):
+    periodo =  models.IntegerField()
+    status_bolsa = models.BooleanField(default=False)
+    valor_bolsa = models.BooleanField(default=False)
+
+class Representante_parceiro(Pessoa):
+    email = models.CharField(max_length=255, null=False)
+    telefone = models.CharField(max_length=255, null=False)
+
+class Projeto(models.Model):
+    nome_projeto = models.CharField(max_length=255, null=False)
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
+    fomento = models.CharField(max_length=255, null=False)
+    
