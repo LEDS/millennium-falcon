2dabdeaf10a7c6a28877081bb025cbbd8a531bd8
Icaro Gavazza <icarodgl@gmail.com>
2017-05-12 18:01:41 -0300

    admin funcionando

--- millenium_falcon/gestao/admin.py
+++ millenium_falcon/gestao/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-from .models import Pessoa, Professor, Servidor, Aluno, Representante_parceiro
-admin.site.register(Pessoa)
+from .models import *
+
+#class ParceiroAdmin(admin.ModelAdmin):
+admin.site.register(Parceiro)
+
+#class PessoaAdmin(admin.ModelAdmin):
+#admin.site.register(Pessoa)
+
+#class ProfessorAdmin(admin.ModelAdmin):
 admin.site.register(Professor)
+
+#class ServidorAdmin(admin.MoedelAdmin):
 admin.site.register(Servidor)
-#admin.site.register(Alunos)
-admin.site.register(Representante_parceiro)
+
+#class ProjetoAdmin(admin.ModelAdmin):
+admin.site.register(Projeto)
 
 class AlunoAdmin(admin.ModelAdmin):
   list_display = ['nome','papel','entrada_leds','periodo']
--- millenium_falcon/gestao/models.py
+++ millenium_falcon/gestao/models.py
@@ -1,31 +1,35 @@
 from __future__ import unicode_literals
 from django.db import models
 
+class Parceiro(models.Model):
+    nome_parceiro = models.CharField(max_length=255)
+    nome_representante = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+
+class Projeto(models.Model):
+    nome_projeto = models.CharField(max_length=255)
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
+    fomento = models.CharField(max_length=255)
+    parceiro_envolvido = models.ForeignKey(Parceiro, on_delete=models.CASCADE, null=True)
+
 class Pessoa(models.Model):
-    nome = models.CharField(max_length=255, null=False)
-    papel = models.CharField(max_length=255, null=False)
+    nome = models.CharField(max_length=255,)
+    papel = models.CharField(max_length=255)
     entrada_leds = models.DateField()
     saida_leds = models.DateField()
-
-class Professor(Pessoa):
-    email = models.CharField(max_length=255, null=False)
-
-
-class Servidor(Pessoa):
-    email = models.CharField(max_length=255, null=False)
+    projeto_envolvido = models.ForeignKey(Projeto, on_delete=models.CASCADE, null=True)
 
 class Aluno(Pessoa):
     periodo =  models.IntegerField()
     status_bolsa = models.BooleanField(default=False)
     valor_bolsa = models.BooleanField(default=False)
 
-class Representante_parceiro(Pessoa):
-    email = models.CharField(max_length=255, null=False)
-    telefone = models.CharField(max_length=255, null=False)
+class Professor(Pessoa):
+    email = models.CharField(max_length=255)
+    setor_vinculado = models.CharField(max_length=255, null=True)
 
-class Projeto(models.Model):
-    nome_projeto = models.CharField(max_length=255, null=False)
-    data_inicio = models.DateField()
-    data_fim = models.DateField()
-    fomento = models.CharField(max_length=255, null=False)
-    
+class Servidor(Pessoa):
+    email = models.CharField(max_length=255)
+    setor_vinculado = models.CharField(max_length=255, null=True)
