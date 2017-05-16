60c4b6339c4b4d8dda245234880646afa7e629c1
ckinupp <ckinupp@gmail.com>
2017-05-16 16:13:19 -0300

    tenta resolver ai o erro de modularizacao

--- millenium_falcon/gestao/models.py
+++ millenium_falcon/gestao/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
+from pessoas.models import *
 
 class Parceiro(models.Model):
     #Na verdade é o Cliente com nome mais bonito
@@ -42,34 +43,6 @@ class Alocacao_bolsa(models.Model):
     inicio = models.DateField(null=True)
     fim = models.DateField(null=True)
     pessoa = models.ForeignKey("Pessoa", null=True)
-
-class Pessoa(models.Model):
-    nome = models.CharField(max_length=255)
-    papel = models.CharField(max_length=255)
-    entrada_leds = models.DateField()
-    saida_leds = models.DateField(blank=True, null=True)
-    projeto_envolvido = models.ManyToManyField(Projeto)
-    email = models.CharField(max_length=255, null=True)
-    telefone = models.CharField(max_length=255, null=True)
-    bolsa = models.ForeignKey("Bolsa",on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return self.nome
-
-class Aluno(Pessoa):
-    periodo_atual =  models.IntegerField()
-    periodo_saida =  models.IntegerField(blank=True, null=True)
-    data_nascimento = models.DateField(null=True)
-    lattes = models.URLField(max_length=255, null=True)
-    cpf = models.CharField(max_length=11,default="")
-    habilidades = models.ManyToManyField("Habilidade")
-
-class Professor(Pessoa):
-    setor_vinculado = models.CharField(max_length=255, null=True)
-
-class Servidor(Pessoa):
-    setor_vinculado = models.CharField(max_length=255, null=True)
-
 
 class Habilidade(models.Model):
     skill = models.CharField(max_length=255, unique=True)
