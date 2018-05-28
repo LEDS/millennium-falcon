d6e72d4ad962e6f05664bd22c65188223806051c
Ivens Augusto  Ferreira Pinto <ivensaugusto@hotmail.com>
2018-05-28 16:16:06 -0300

    ...

--- MilleniumFalcon/produtos/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Atividade
-
-
-class AtividadeAdmin(admin.ModelAdmin):
-    list_display = ['nome','tipo' , 'local', 'data']
-    list_filter = ['tipo','local', 'data']
-admin.site.register(Atividade,AtividadeAdmin)
-
